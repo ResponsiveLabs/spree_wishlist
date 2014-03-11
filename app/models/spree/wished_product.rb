8663b679d4e7a9c593dd88d69152f66156fed09f
Daniel Garcia <daniel@responsivelabs.mx>
2014-03-11 17:42:06 -0600

    BEB-220 Add initial_quantity to Spree::WishedProduct

--- app/models/spree/wished_product.rb
+++ app/models/spree/wished_product.rb
@@ -2,7 +2,14 @@ class Spree::WishedProduct < ActiveRecord::Base
   belongs_to :variant
   belongs_to :wishlist
   
-  attr_accessible :wishlist_id, :variant, :variant_id, :quantity
+  attr_accessible :wishlist_id, :variant, :variant_id, :quantity, :initial_quantity
 
+  before_create :set_default_values
 
+  private
+
+  def set_default_values
+    self.quantity = 1 unless quantity
+    self.initial_quantity = quantity
+  end
 end
